336a2204690c37ceb508e4a015244c25be434951
codercloudX <cloud562935456@gmail.com>
2020-12-24 14:03:14 +0800

    first commit

--- 1.基础/7.循环_for循环9x9.py
+++ 1.基础/7.循环_for循环9x9.py
@@ -2,5 +2,5 @@ end=10
 start=0
 for i in range(start,end):
     for j in range(1,i+1):
-      print("%d * %d = %d" % (j,i,i*j),end="\t")
+      print("%d * %d = %d" % (j,i,i*j),end="\t")#不换行
     print()
--- 6.python爬虫/0.Beautiful Soup的使用.py
+++
@@ -1,8 +0,0 @@
-# 安装Beautiful Soup
-#   pip3 install bs4 -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
-from bs4 import BeautifulSoup
-import requests
-soup=BeautifulSoup("https://www.qiushibaike.com/text/","html.parser")
-soup.find('div')
-
-
